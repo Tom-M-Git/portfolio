83d0b7eabfe0767285dc2dbb338a1285c21591ad
Tom-M-Git <astral.taney@gmail.com>
2020-11-11 01:15:36 +0900

    the 1st commit for rebuilding

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import Gettext from '../lib/gettext.js';
+import Header from './components/header.js';
+import Footer from './components/footer.js';
+
+let jaJsonUrl, enJsonUrl, rootEl;
+jaJsonUrl = "../locale/ja.json";
+enJsonUrl = "../locale/en-US.json";
+rootEl = document.getElementById("root");
---
+++ projects/development/development.js
@@ -0,0 +1,3 @@
+import Gettext from '../../lib/gettext.js';
+import Header from '../../js/components/header.js';
+import Footer from '../../js/components/footer.js';
