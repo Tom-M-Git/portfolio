65a69528895636d47fa59fa6673b1be68a6f1b70
Tom-M-Git <tomoaki.morioka.email@gmail.com>
2020-11-29 17:58:08 +0900

    fixes

--- js/home/projects/components/main.js
+++ js/home/projects/components/main.js
@@ -198,6 +198,8 @@ function Main () {
                             align-items: center;
                             position: fixed;
                             top: 0;
+                            left: 0;
+                            right: 0;
                             width: 100%;
                             height: 100%;
                             z-index: 997;
